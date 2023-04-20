1f131549dacd95b15f4b1689093e8e80de10d470
wesleymichael <wesleymichaelpereira@hotmail.com>
2023-04-20 02:30:09 -0300

    Functionality: register user

--- src/App.js
+++ src/App.js
@@ -24,5 +24,5 @@ const PagesContainer = styled.main`
   background-color: #8c11be;
   width: calc(100vw - 50px);
   max-height: 100vh;
-  padding: 25px;
+  padding: 0 25px;
 `
--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -1,20 +1,88 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import styled from "styled-components"
 import MyWalletLogo from "../components/MyWalletLogo"
+import { useState } from "react"
+import axios from 'axios'
 
 export default function SignUpPage() {
+  const [form, setForm] = useState({name: "", email: "", password: "", confirmPassword: ""});
+  const [disableForm, setDisableForm] = useState(false);
+  const navigate = useNavigate();
+  const BASE_URL = process.env.REACT_APP_API_URL;
+  
+  function handleForm(e){
+    setForm({...form, [e.target.name]: e.target.value});
+  }
+
+  function register(e){
+    e.preventDefault();
+    setDisableForm(true);
+
+    if(form.password !== form.confirmPassword){
+      alert('Confirmação de senha não confere')
+    } else {
+      const body = { name: form.name, email: form.email, password: form.password };
+      axios.post(`${BASE_URL}/cadastro`, body)
+        .then( () => {
+          navigate("/");
+        })
+        .catch( error => {
+          alert(error.response.data.message);
+          setDisableForm(false);
+        });
+    }
+  }
+  
   return (
     <SingUpContainer>
-      <form>
+      <form onSubmit={register}>
         <MyWalletLogo />
-        <input placeholder="Nome" type="text" />
-        <input placeholder="E-mail" type="email" />
-        <input placeholder="Senha" type="password" autocomplete="new-password" />
-        <input placeholder="Confirme a senha" type="password" autocomplete="new-password" />
-        <button>Cadastrar</button>
+        <input 
+          id="name"
+          type="text"
+          placeholder="Nome"
+          name="name"
+          value={form.name}
+          onChange={handleForm}
+          disabled={disableForm}
+          required
+        />
+        <input 
+          id="email"
+          type="email"
+          placeholder="E-mail"
+          name="email"
+          value={form.email}
+          onChange={handleForm}
+          disabled={disableForm}
+          required
+        />
+        <input
+          id="password" 
+          type="password"
+          placeholder="Senha"
+          name="password"
+          value={form.password}
+          onChange={handleForm}
+          disabled={disableForm}
+          autoComplete="new-password"
+          required
+        />
+        <input
+          id="confirmPassword" 
+          type="password"
+          placeholder="Confirme a senha"
+          name="confirmPassword"
+          value={form.confirmPassword}
+          onChange={handleForm}
+          disabled={disableForm}
+          autoComplete="new-password"
+          required
+        />
+        <button type="submit">Cadastrar</button>
       </form>
 
-      <Link>
+      <Link to="/">
         Já tem uma conta? Entre agora!
       </Link>
     </SingUpContainer>
